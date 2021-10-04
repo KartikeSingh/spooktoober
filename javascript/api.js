1b201620d8b3150d1749bf86b5407900278408a2
Tejas Lamba <73745640+TajuModding@users.noreply.github.com>
2021-10-04 21:17:08 +0530

    Update api.js

--- javascript/api.js
+++ javascript/api.js
@@ -1,1 +1,15 @@
-
+const express = require('express')
+const app = express()
+// Simple YT Vid downloader 
+// Ex. https://url.domain/api?url=https://www.youtube.com/watch?v=dQw4w9WgXcQ
+app.get('/api', (res, req) => {
+res.redirect(`https://cryptons.ga/api/v1/youtube/download?url=${req.query.url}`)
+})
+app.get('/', (res, req) => {
+res.send({
+gay; "Why u even here?"
+})
+})
+app.listen(3000, () => {
+  console.log('server started');
+});
